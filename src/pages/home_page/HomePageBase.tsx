99bf50344bc9eb57e60b71f56b66b93d0ffc57fb
Eduardonn <eduardof1999@hotmail.com>
2023-04-29 23:18:27 -0300

    Changed section's margins on mobile

--- src/pages/home_page/HomePageBase.tsx
+++ src/pages/home_page/HomePageBase.tsx
@@ -3,6 +3,8 @@ import { JSX } from "solid-js"
 
 export const HomePageSection = ({ children }: { children: JSX.Element }) => {
   return (
-    <section class='my-6 mx-[5vw] sm:mx-[15vw] p-4 border-corner-gradient'>{children}</section>
+    <section class='my-6 mx-[2vw] md:mx-[15vw] p-4 border-corner-gradient'>
+      {children}
+    </section>
   )
 }
