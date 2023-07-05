e8ff7da23d8236f54077d5bd02fefd5f5159fec3
Eduardonn <eduardof1999@hotmail.com>
2023-07-05 18:48:53 -0300

    Added ScrollTopButton

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import { Component } from 'solid-js';
 import { Routes, Route, Router } from '@solidjs/router';
 import Home from './pages/home_page';
-import MemorizationAppPage from './pages/project_pages/MemorizationApp/MemorizationAppPage';
+import RememoPage from './pages/project_pages/RememoPage/RememoPage';
 import AlgorithmVisualizerPage from './pages/project_pages/SortingAlgorithmsVisualizerPage/SortingAlgorithmsVisualizerPage';
-import SnippingToolDigitizerPage from './pages/project_pages/SnippingToolDigitizerPage/SnippingToolDigitizerPage';
+import ScreenshotDigitizerPage from './pages/project_pages/ScreenshotDigitizerPage/ScreenshotDigitizerPage';
 import PortfolioPage from './pages/project_pages/PortfolioPage/PortfolioPage';
 import CamSurSystemPage from './pages/project_pages/CamSurSystemPage/CamSurSystemPage';
+import ScrollTopButton from './common_components/ScrollTopButton';
 
 const App: Component = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' component={Home} />
-        <Route path='/projects/memorization-app' component={MemorizationAppPage} />
-        <Route path='/projects/3d-sorting-algorithms-visualizer' component={AlgorithmVisualizerPage} />
-        <Route path='/projects/snipping-tool-digitizer' component={SnippingToolDigitizerPage} />
-        <Route path='/projects/camera_surveillance_system' component={CamSurSystemPage} />
-        <Route path='/projects/portfolio' component={PortfolioPage} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' component={Home} />
+          <Route path='/projects/rememo' component={RememoPage} />
+          <Route path='/projects/3d-sorting-algorithms-visualizer' component={AlgorithmVisualizerPage} />
+          <Route path='/projects/screenshot-digitizer' component={ScreenshotDigitizerPage} />
+          <Route path='/projects/camera_surveillance_system' component={CamSurSystemPage} />
+          <Route path='/projects/portfolio' component={PortfolioPage} />
+        </Routes>
+      </Router>
+      <ScrollTopButton />
+    </>
   );
 }
 
---
+++ src/common_components/ScrollTopButton.tsx
@@ -0,0 +1,28 @@
+import { createSignal } from "solid-js";
+
+export const ScrollTopButton = () => {
+  const [visible, setVisible] = createSignal(false);
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
+      setVisible(true);
+    } else {
+      setVisible(false);
+    }
+  });
+
+  return (
+    <button
+      style={visible() ? 'opacity: 1' : 'opacity: 0; pointer-events: none'}
+      class='fixed bottom-8 right-8 w-14 h-14
+        font-[verdana] text-xl
+        bg-gray-100 rounded-full shadow-md
+        transition-opacity duration-500'
+      onClick={() => window.scrollTo({ top: 0, behavior: 'smooth'})}
+    >
+      ^
+    </button>
+  );
+}
+
+export default ScrollTopButton;
